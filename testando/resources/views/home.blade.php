c5a45b43d00a845a48890606dc60bc632c29cad1
larissafelixl <larissafelixpalhano@gmail.com>
2024-04-28 21:48:12 -0300

    estilização do welcome com tailwind

--- testando/resources/views/home.blade.php
+++ testando/resources/views/home.blade.php
@@ -3,4 +3,6 @@
 @section('content')
 <button class="btn btn-success btn-sm">button</button>
 
+<h1 class="bg-bubble-gum text-white mt-6"> Um título</h1>
+
   @endsection
--- testando/tailwind.config.js
+++ testando/tailwind.config.js
@@ -7,6 +7,14 @@ export default {
     ],
     theme: {
       extend: {},
+
+      colors: {
+        'high-purplle':'#1B132D',
+        'orange': '#FC9A03',
+        'gray': '#C1C1C1',
+        'purplle':'#563274'
+
+      },
     },
     plugins: [],
   }
